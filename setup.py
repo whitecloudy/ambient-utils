ebf67506d7318bd75c4369ec4d9ccd7861bbbefb
giannisdaras <daras.giannhs@gmail.com>
2025-07-23 21:44:19 -0500

    Decrease deps

--- setup.py
+++ setup.py
@@ -6,12 +6,40 @@ root = Path(__file__).parent.resolve()
 with open(root / "README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open(root / "requirements.txt", "r", encoding="utf-8") as f:
-    requirements = [line.strip() for line in f.readlines()]
+# Define core dependencies (minimal set)
+core_requirements = [
+    "numpy",
+    "scipy",
+    "torch",
+    "Pillow",
+]
+
+# Define optional dependencies for full functionality
+extras_require = {
+    "all": [
+        "wandb",
+        "seaborn", 
+        "torchvision",
+        "diffusers",
+        "s3fs",
+        "webdataset",
+        "datasets",
+        "imagecorruptions",
+    ],
+    "dev": [
+        "pytest",
+        "black",
+        "flake8",
+    ],
+    "docs": [
+        "sphinx",
+        "sphinx-rtd-theme",
+    ],
+}
 
 setuptools.setup(
     name="ambient_utils",
-    version="1.0.1",
+    version="1.0.2",
     author="giannisdaras",
     author_email="daras.giannhs@gmail.com",
     description="Utility functions for learning generative models from corrupted data.",
@@ -19,7 +47,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/giannisdaras/ambient_utils",
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    install_requires=core_requirements,
+    extras_require=extras_require,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
